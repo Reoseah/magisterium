ad434f5c9dba5c330205676a94f8d802210b285a
reoseah <reoseah9@gmail.com>
2024-11-04 00:27:36 +0100

    add particles to Glyphic Ignition

--- src/main/java/io/github/reoseah/magisterium/data/effect/GlyphicIgnitionEffect.java
+++ src/main/java/io/github/reoseah/magisterium/data/effect/GlyphicIgnitionEffect.java
@@ -3,17 +3,23 @@ package io.github.reoseah.magisterium.data.effect;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import io.github.reoseah.magisterium.block.GlyphBlock;
+import io.github.reoseah.magisterium.network.SpellParticlePayload;
 import io.github.reoseah.magisterium.recipe.SpellRecipeInput;
 import io.github.reoseah.magisterium.world.MagisteriumPlaygrounds;
+import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.dynamic.Codecs;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+
+import java.util.ArrayList;
 
 public class GlyphicIgnitionEffect extends SpellEffect {
     public static final MapCodec<GlyphicIgnitionEffect> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group( //
@@ -36,33 +42,41 @@ public class GlyphicIgnitionEffect extends SpellEffect {
 
     @Override
     public void finish(SpellRecipeInput input, RegistryWrapper.WrapperLookup lookup) {
-        boolean hasTargets = false;
-        boolean hasLit = false;
-        boolean hasFailed = false;
+        var targets = new ArrayList<BlockPos>();
+        boolean hasSuccess = false;
+        boolean hasFailure = false;
 
         World world = input.player.getWorld();
         BlockPos center = input.player.getBlockPos();
         for (BlockPos pos : BlockPos.iterateOutwards(center, this.maxRange, this.maxRange, this.maxRange)) {
             BlockState state = world.getBlockState(pos);
             if (state.isOf(GlyphBlock.INSTANCE)) {
-                hasTargets = true;
+                targets.add(pos.toImmutable());
                 if (MagisteriumPlaygrounds.trySetBlockState(world, pos, Blocks.FIRE.getDefaultState(), input.player)) {
-                    hasLit = true;
+                    hasSuccess = true;
                 } else {
-                    hasFailed = true;
+                    hasFailure = true;
                 }
             }
         }
-
-        if (!hasTargets) {
-            input.player.sendMessage(Text.translatable("magisterium.gui.no_glyphs_found"), true);
+        if (!targets.isEmpty()) {
+            var payload = new SpellParticlePayload(targets);
+            for (var player : PlayerLookup.tracking((ServerWorld) world, center)) {
+                ServerPlayNetworking.send(player, payload);
+            }
+        } else {
+            input.player.sendMessage(Text.translatable("magisterium.gui.no_targets"), true);
             ((ServerPlayerEntity) input.player).closeHandledScreen();
-        } else if (hasFailed && hasLit) {
-            input.player.sendMessage(Text.translatable("magisterium.gui.partial_success"), true);
-        } else if (hasFailed) {
-            input.player.sendMessage(Text.translatable("magisterium.gui.no_success"), true);
-            ((ServerPlayerEntity) input.player).closeHandledScreen();
+            return;
         }
 
+        if (hasFailure) {
+            if (hasSuccess) {
+                input.player.sendMessage(Text.translatable("magisterium.gui.partial_success"), true);
+            } else {
+                input.player.sendMessage(Text.translatable("magisterium.gui.no_success"), true);
+                ((ServerPlayerEntity) input.player).closeHandledScreen();
+            }
+        }
     }
 }
