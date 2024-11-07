b1389fba20295372c835ec22f6d13ceeb5140082
reoseah <reoseah9@gmail.com>
2024-11-07 09:30:06 +0100

    make conflagrate spell scale with ingredients again

--- src/main/java/io/github/reoseah/magisterium/data/effect/ConflagrateEffect.java
+++ src/main/java/io/github/reoseah/magisterium/data/effect/ConflagrateEffect.java
@@ -8,23 +8,51 @@ import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.ConnectingBlock;
+import net.minecraft.recipe.Ingredient;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Pair;
 import net.minecraft.util.dynamic.Codecs;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 
+import java.util.List;
+
 public class ConflagrateEffect extends SpellEffect {
+    private static final MapCodec<Pair<Ingredient, Integer>> BONUS_CODEC = RecordCodecBuilder.mapCodec(bonus -> bonus.group( //
+            Ingredient.CODEC.fieldOf("ingredient").forGetter(Pair::getLeft), //
+            Codecs.POSITIVE_INT.fieldOf("range_increase").forGetter(Pair::getRight) //
+    ).apply(bonus, Pair::new));
+
     public static final MapCodec<ConflagrateEffect> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group( //
             Identifier.CODEC.fieldOf("utterance").forGetter(effect -> effect.utterance), //
-            Codecs.POSITIVE_INT.fieldOf("duration").forGetter(effect -> effect.duration) //
+            Codecs.POSITIVE_INT.fieldOf("duration").forGetter(effect -> effect.duration), //
+            Codecs.POSITIVE_INT.fieldOf("offset").forGetter(effect -> effect.offset), //
+            Codecs.POSITIVE_INT.fieldOf("range").forGetter(effect -> effect.range), //
+            Codecs.POSITIVE_INT.fieldOf("ramp_length").forGetter(effect -> effect.rampLength), //
+            BONUS_CODEC.codec().listOf().fieldOf("bonuses").forGetter(effect -> effect.bonuses) //
     ).apply(instance, ConflagrateEffect::new));
 
-    public ConflagrateEffect(Identifier utterance, int duration) {
+    public final int offset;
+    public final int range;
+    public final int rampLength;
+    public final List<Pair<Ingredient, Integer>> bonuses;
+
+    public ConflagrateEffect(Identifier utterance,
+                             int duration,
+                             int offset,
+                             int range,
+                             int rampLength,
+                             List<Pair<Ingredient, Integer>> bonuses
+    ) {
         super(utterance, duration);
+        this.offset = offset;
+        this.range = range;
+        this.rampLength = rampLength;
+        this.bonuses = bonuses;
     }
 
     @Override
@@ -34,25 +62,21 @@ public class ConflagrateEffect extends SpellEffect {
 
     @Override
     public void finish(SpellRecipeInput input, RegistryWrapper.WrapperLookup lookup) {
-        // TODO: extract this to a helper method, reuse in other recipes
-        int totalValue = 0;
-//        for (int i = 0; i < input.size(); i++) {
-//            var stack = input.getStackInSlot(i);
-//            if (!stack.isEmpty()) {
-//                int value = ItemValuesLoader.getValue(stack);
-//                totalValue += value;
-//
-//                stack.decrement(1);
-//                input.inventory.setStack(i, stack);
-//            }
-//        }
+        int bonusRange = 0;
+        for (int i = 0; i < input.size(); i++) {
+            var stack = input.getStackInSlot(i);
+            if (!stack.isEmpty()) {
+                for (var bonus : bonuses) {
+                    if (bonus.getLeft().test(stack)) {
+                        bonusRange += bonus.getRight();
+                        stack.decrement(1);
+                        break;
+                    }
+                }
+            }
+        }
 
-        // TODO spawn a bunch of fire particles in the area
-
-        final int buildUpStart = 1, buildUpFinish = 5;
-
-        int decayStart = 5 + totalValue;
-        int decayFinish = 9 + totalValue;
+        int max = this.offset + this.range + bonusRange;
 
         boolean hasTargets = false;
         boolean hasLit = false;
@@ -60,24 +84,13 @@ public class ConflagrateEffect extends SpellEffect {
 
         var world = input.player.getWorld();
         var center = input.player.getBlockPos();
-        for (var pos : BlockPos.iterate(center.add(-decayFinish, -decayFinish, -decayFinish), center.add(decayFinish, decayFinish, decayFinish))) {
+        for (var pos : BlockPos.iterateOutwards(center, max, max, max)) {
             var state = world.getBlockState(pos);
             var block = state.getBlock();
             var entry = FlammableBlockRegistry.getInstance(Blocks.FIRE).get(block);
 
             double distance = Math.sqrt(center.getSquaredDistance(pos));
-            double chance;
-            if (distance < buildUpStart) {
-                chance = 0;
-            } else if (distance < buildUpFinish) {
-                chance = (distance - buildUpStart) / (buildUpFinish - buildUpStart);
-            } else if (distance < decayStart) {
-                chance = 1;
-            } else if (distance < decayFinish) {
-                chance = 1 - (distance - decayStart) / (decayFinish - decayStart);
-            } else {
-                chance = 0;
-            }
+            double chance = getChance(distance, this.offset, this.offset + this.rampLength, max - this.rampLength, max);
 
             if (entry != null && entry.getBurnChance() > 0) {
                 if (world.random.nextFloat() < chance) {
@@ -105,6 +118,20 @@ public class ConflagrateEffect extends SpellEffect {
         } else if (hasFailed) {
             input.player.sendMessage(Text.translatable("magisterium.gui.no_success"), true);
             ((ServerPlayerEntity) input.player).closeHandledScreen();
+        }
+    }
+
+    private static double getChance(double distance, int min, int plateauStart, int plateauEnd, int max) {
+        if (distance < min) {
+            return 0;
+        } else if (distance < plateauStart) {
+            return (distance - min) / (plateauStart - min);
+        } else if (distance < plateauEnd) {
+            return 1;
+        } else if (distance < max) {
+            return 1 - (distance - plateauEnd) / (max - plateauEnd);
+        } else {
+            return 0;
         }
     }
 
